cbe0f3c45e2bff6df18e4fdca815df2584a0b02e
mvkras <avkras1992@gmail.com>
2020-03-20 14:05:30 +1000

    Created Human logics

--- GuessTheNumber/GuessNumberGame.cs
+++ GuessTheNumber/GuessNumberGame.cs
@@ -21,5 +21,67 @@ namespace A_GuessTheNumber
             this.maxTries = maxTries;
             this.guessingPlayer = guessingPlayer;
         }
+        public void Start() //Метод для запуска игры
+        {
+            if(guessingPlayer == GuessingPlayer.Human)
+            {
+                HumanGuesses(); //если выбран человек, человек отгадывет
+            }
+            else
+            {
+                ComputerGuesses(); //если выбран компьютер, комп отгадывет
+            }
+        }
+
+        private void HumanGuesses() //логика когда отгадывает человек
+        {
+            //Сперва компьютер должен загодать число
+            Random rand = new Random();
+            int guessNumber = rand.Next(0, max); //Загаданное число в пределах от 0 до 100            
+           
+            int lastGuess = -1; // Заведем крайняя попытка(последнее введеное число пользователем)  по умолчанию равен -1          
+            int tries = 0; //заведем счетчик
+            Console.WriteLine("Правила: Угадайте число от 0 до 100, у вас есть 6 попыток!");
+            Console.WriteLine("Введите число от 0 до 100:");
+            string str = Console.ReadLine();
+            bool result = int.TryParse(str, out lastGuess);
+            if(result)
+            {
+                //Игра будет идти до тех пор, пока пользователь не угадал число, либо не исчерпано количество попыток          
+                while (lastGuess != guessNumber && tries < maxTries)
+                {
+                    if(lastGuess == guessNumber)
+                    {
+                        Console.WriteLine($"Верно, вы угадали! это число: {guessNumber}.");
+                        break;
+                    }
+                    else if(lastGuess < guessNumber)
+                    {
+                        Console.WriteLine($"Мало! Твое число меньше, загаданного! Попытка: {tries}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Перебор! Твои число больше загаданного! Попытка: {tries}.");
+                    }
+                    tries++; //Количество попыток увеличивается с каждым ходом, достигнет 6, игра окончена
+                    if(tries == maxTries)
+                    {
+                        Console.WriteLine("Game Over! Ты не отгадал за 6 попыток!");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число, попробуйте еще раз.");
+            }
+           
+        }
+
+
+        private void ComputerGuesses() //логика, когда отгадывает компьютер
+        { 
+
+        }
     }
 }

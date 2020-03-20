130fba87c859b8ca69647fd551f52f1fb8455acd
mvkras <avkras1992@gmail.com>
2020-03-20 13:35:07 +1000

    Creating GuessTheNumber game

---
+++ B_Tic_Tac_Toe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B_Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GuessTheNumber/GuessNumberGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A_GuessTheNumber
+{
+    enum GuessingPlayer //выбираем, кто будет играть
+    {
+        Human,
+        Computer
+    }
+    class GuessNumberGame
+    {      
+        private readonly int max;
+        private readonly int maxTries;
+        private readonly GuessingPlayer guessingPlayer;
+        //создаем конструктор с полями:
+        public GuessNumberGame(int max = 100, int maxTries = 6, GuessingPlayer guessingPlayer = GuessingPlayer.Human)
+        {
+            this.max = max;
+            this.maxTries = maxTries;
+            this.guessingPlayer = guessingPlayer;
+        }
+    }
+}

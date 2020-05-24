9052d2f43d5c18d674f87833670a718d702d4417
mvkras <avkras1992@gmail.com>
2020-05-24 14:41:45 +1000

    fixed problems with IMT, added info about numbers

--- IndexMassOfBody/Program.cs
+++ IndexMassOfBody/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace IndexMassOfBody
 {
@@ -6,7 +8,8 @@ namespace IndexMassOfBody
     {
         static void Main(string[] args)
         {
-            BodyMassIndex(); //Рассчет "Индекс массы тела"
+           BodyMassIndex(); //Рассчет "Индекс массы тела"
+         //   ReadResultsOfIMT(); //чтение результатов
         }
         static void BodyMassIndex()
         {
@@ -82,39 +85,71 @@ namespace IndexMassOfBody
             Console.WriteLine();
             double IMT = resultWeight / (resultHeight / 100 * resultHeight / 100);
             IMT = Math.Round(IMT, 2);
-            Console.WriteLine("*** Ваш результат: ***");
 
-            Console.Write($"Ваше имя: {name},\nВозраст: {ageResult} лет,\nИндекс массы тела равен: {IMT} - ");
 
+            //   Console.Write(fullResultOfIMT);
+            //  Console.WriteLine("*** Ваш результат: ***");
+
+            //Console.Write($"Ваше имя: {name},\nВозраст: {ageResult} лет,\nИндекс массы тела равен: {IMT} - ");
+            string totalResult = null;          
+            string fullResultOfIMT = null; 
             if (IMT <= 16)
             {
-                Console.Write("Выраженный дефицит массы тела");
+                totalResult = "Выраженный дефицит массы тела";
+             
             }
             else if (IMT > 16 && IMT <= 18.5)
             {
-                Console.Write("Недостаточная масса тела");
+                totalResult = "Недостаточная масса тела";
+              
             }
             else if (IMT > 18.5 && IMT <= 24.99)
             {
-                Console.Write("Показатели в норме");
+                totalResult = "Показатели в норме";
+             
             }
             else if (IMT >= 25 && IMT <= 30)
             {
-                Console.Write("Избыточная масса тела (ожирение)");
+                totalResult = "Избыточная масса тела (ожирение)";
+              
             }
             else if (IMT > 30 && IMT <= 35)
             {
-                Console.Write("Ожирение первой степени");
+                totalResult = "Ожирение первой степени";
+               
             }
             else if (IMT > 35 && IMT <= 40)
             {
-                Console.Write("Ожирение второй степени");
+                totalResult = "Ожирение второй степени";
+               
             }
             else if (IMT > 40)
             {
-                Console.Write("Ожирение третьей степени (морбидное)");
+                totalResult = "Ожирение третьей степени (морбидное)";
+              
             }
+            
+            string yourResultOfIMT = "\n*** Ваш результат: ***\n";
+            fullResultOfIMT = yourResultOfIMT + $"Ваше имя: {name},\nВозраст: {ageResult} лет,\nИндекс массы тела равен: {IMT} - " + totalResult;
+            Console.WriteLine(fullResultOfIMT);
             Console.WriteLine();
+            
+          
+            //запись результата в файл
+            using (StreamWriter writeResult = new StreamWriter(@"C:\Users\User\Desktop\IMT.txt", true, Encoding.UTF8))
+            {
+                 writeResult.WriteLine(fullResultOfIMT);                   
+            }
+           
+        }
+
+        static void ReadResultsOfIMT()
+        {
+            using (StreamReader readResult = new StreamReader(@"C:\Users\User\Desktop\IMT.txt", Encoding.UTF8))
+            {
+                string readingText = readResult.ReadToEnd();
+                Console.WriteLine(readingText);
+            }
         }
     }
 }
